4fbdde9fda1ead77b9ffe6273655ee97aa5ab822
keshav <paruchuri93@gmail.com>
2017-07-06 15:56:14 +0000

    friends list added

--- PostDemo/app.js
+++ PostDemo/app.js
@@ -7,6 +7,13 @@ app.get("/", function(req,res){
    res.render("home.ejs"); 
 });
 
+app.get("/friends", function(req, res){
+   var friends =[bob, kim, joe, chris, ross];
+   
+   res.render("friendsPage.ejs");
+   
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Sever from PostDemo");
 });
