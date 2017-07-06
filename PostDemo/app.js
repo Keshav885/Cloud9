08d5fe8c61e8ede2ee7ffdfd16ee6285e42efe04
keshav <paruchuri93@gmail.com>
2017-07-06 18:43:48 +0000

    added new friend name to same page

--- PostDemo/app.js
+++ PostDemo/app.js
@@ -1,17 +1,26 @@
 var express =require("express");
 var app =express();
+var bodyParser =require("body-parser");
 
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+var friends =["bob", "kim", "joe", "chris", "ross"];
 
 app.get("/", function(req,res){
    res.render("home.ejs"); 
 });
 
 app.get("/friends", function(req, res){
-   var friends =["bob", "kim", "joe", "chris", "ross"];
-   
    res.render("friendsPage.ejs", {friends: friends});
    
+});
+
+app.post("/newfriend", function(req, res){
+   var addnew = req.body.addnew;
+   friends.push(addnew);
+   res.redirect("/friends");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
